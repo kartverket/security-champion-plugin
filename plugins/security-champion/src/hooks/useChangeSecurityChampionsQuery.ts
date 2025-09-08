304b0d50b325a15a2d6fe12f97080adcbef365ce
Erik Torvbråten <erik.torvbraten@bekk.no>
2025-09-08 16:04:38 +0200

    legg til entra id i API kall

--- plugins/security-champion/src/hooks/useChangeSecurityChampionsQuery.ts
+++ plugins/security-champion/src/hooks/useChangeSecurityChampionsQuery.ts
@@ -2,23 +2,28 @@ import { useMutation } from "@tanstack/react-query"
 
 import { SecurityChamp } from "../types"
 import { post } from "../api/client"
-import { getBackstageToken } from "../utils/authenticationUtils"
-import { configApiRef, identityApiRef, useApi } from "@backstage/core-plugin-api"
+import { getAuthenticationTokens } from "../utils/authenticationUtils"
+import { configApiRef, identityApiRef, microsoftAuthApiRef, useApi } from "@backstage/core-plugin-api"
 export const useSetSecurityChampionMutation = () => {
 
     const backendUrl = useApi(configApiRef).getString('backend.baseUrl');
     const backstageAuthApi = useApi(identityApiRef)
-        
+
+    const microsoftAuthApi = useApi(microsoftAuthApiRef)
+    const config = useApi(configApiRef)
+
     return useMutation({
         mutationFn: async (securityChampion : SecurityChamp) => {
-            const { backstageToken } =
-                            await getBackstageToken(
+            const { entraIdToken, backstageToken } =
+                            await getAuthenticationTokens(
+                                config,
                                 backstageAuthApi,
+                                microsoftAuthApi
                             )
 
             const endpointUrl = backendUrl + "/api/proxy/security-champion-proxy/api/setSecurityChampion"
 
-            return post<SecurityChamp, string>( endpointUrl, backstageToken, securityChampion )
+            return post<{securityChampion : SecurityChamp ;  entraIdToken : string}, string>( endpointUrl, backstageToken,  {securityChampion, entraIdToken})
         },
     })
 }
--- plugins/security-champion/src/hooks/useSecurityChampionsQuery.ts
+++ plugins/security-champion/src/hooks/useSecurityChampionsQuery.ts
@@ -2,24 +2,29 @@ import { useQuery } from "@tanstack/react-query"
 
 import { SecurityChamp } from "../types"
 import { post } from "../api/client"
-import { configApiRef, identityApiRef, useApi } from "@backstage/core-plugin-api"
-import { getBackstageToken } from "../utils/authenticationUtils"
+import { configApiRef, identityApiRef, microsoftAuthApiRef, useApi } from "@backstage/core-plugin-api"
+import { getAuthenticationTokens } from "../utils/authenticationUtils"
 
 export const useSecurityChampionsQuery = (repositoryNames: string[]) => {
     const backendUrl = useApi(configApiRef).getString('backend.baseUrl');
     const backstageAuthApi = useApi(identityApiRef)
+
+    const microsoftAuthApi = useApi(microsoftAuthApiRef)
+    const config = useApi(configApiRef)
         
     return useQuery<SecurityChamp[], Error>({
         queryKey: ["security-champions", repositoryNames],
         queryFn: async () => {
 
-            const { backstageToken } =
-                await getBackstageToken(
+            const { entraIdToken, backstageToken } =
+                await getAuthenticationTokens(
+                    config,
                     backstageAuthApi,
+                    microsoftAuthApi
                 )
             
             const endpointUrl = backendUrl + "/api/proxy/security-champion-proxy/api/securityChampion"
-            return post<{repositoryNames: string[] }, SecurityChamp[]>(endpointUrl, backstageToken, { repositoryNames } )
+            return post<{repositoryNames: string[]; entraIdToken:string }, SecurityChamp[]>(endpointUrl, backstageToken, { repositoryNames, entraIdToken } )
         },
         enabled: repositoryNames.length !== 0,
         staleTime: 3600000,
